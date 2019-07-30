c4a9dbb5fddbb209347dc44d02b57351ada87a1c
maximoose773 <48160441+maximoose773@users.noreply.github.com>
2019-07-30 21:37:10 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactNative from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "C:ProjectsyokkaoProjectsrccomponentsheader";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
+}
